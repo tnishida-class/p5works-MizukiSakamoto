1c0aec522db8818a7756f6ec2fa863192dfdc981
MizukiSakamoto <72115830+MizukiSakamoto@users.noreply.github.com>
2020-10-20 12:17:18 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
+  createCanvas(100,100);
+  noFill();
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    ellipse(50,50,i*10,i*10);
+  if (i<5) {
+     stroke(0,0,255);
+   }
+  else {
+    stroke(255,0,0);
   }
+}
 }
