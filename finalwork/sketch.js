966d30ee4715cdf957406afb69052f1da2d37170
MizukiSakamoto <72115830+MizukiSakamoto@users.noreply.github.com>
2020-11-23 21:32:27 +0900

    Update finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,44 @@
 // 最終課題を制作しよう
 
-function setup(){
+let x,y,vx,vy,angle;
+const g =1;
+const vyMax = 30;
+function setup() {
   createCanvas(windowWidth, windowHeight);
+ x = width/2;
+ y = height/2;
+  vx=8;
+  vy=8;
+  angle=0;
 }
 
-function draw(){
-  background(160, 192, 255);
+function draw() {
+  background(0);
+  noStroke();
+  fill('yellow');
+  star(x,y,50,angle);
+  angle += 0.1;
+  x += vx;
+  y += vy;
+
+  vy = constrain(vy+g,-vyMax,vyMax);
+  if(x<0||x>width){vx=-1*vx;}
+  if(y>height){vy=-1*vy;}
+  x = constrain(x,0,width);
+  y = constrain(y,0,height);
+}
+
+function star(cx,cy,r,angle){
+  beginShape();
+  for(var i = 0;i<20;i++){
+    var theta=TWO_PI*i*2/5-HALF_PI+angle;
+    var x = cx+cos(theta)*r;
+    var y = cy+sin(theta)*r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
 }
 
 function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth,windowHeight);
 }
